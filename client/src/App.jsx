e2c7cd7a23f388b8ea00303b0cd8d757c4990939
Vivek Korah <lcb2021051@iiitl.ac.in>
2024-11-08 12:48:15 +0530

    Added Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/about";
+import Home from "./pages/home";
+import Profile from "./pages/profile";
+import SignIn from "./pages/signIn";
+import SignOut from "./pages/SignUp";
 
 function App() {
-  return <h1 className="text-red-600 font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignOut />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
