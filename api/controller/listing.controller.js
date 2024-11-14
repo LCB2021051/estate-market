42d2d46a131fe5ccceb5343e12f93fdd4038bddf
Vivek Korah <lcb2021051@iiitl.ac.in>
2024-11-14 23:13:08 +0530

    Update Listing api

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -16,11 +16,33 @@ export const deleteListing = async (req, res, next) => {
     return next(errorHandler(404, "Listing not found!"));
   }
   if (req.user.id !== listing.userRef) {
-    return next(errorHandler(400, "You can't delete this listing!"));
+    return next(errorHandler(401, "You can't delete this listing!"));
   }
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json({ "message:": "Listing Deleted Succesfully!" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(
+      errorHandler(401, "You're not authorized to update this listing!")
+    );
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
