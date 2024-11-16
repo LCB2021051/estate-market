8c192e56169883e9db1404090369b8db14fcc048
Vivek Korah <lcb2021051@iiitl.ac.in>
2024-11-16 16:47:33 +0530

    Search page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">realstate</span>
+            <span className="text-slate-500">realestate</span>
             <span className="text-slate-700">Market</span>
           </h1>
         </Link>
